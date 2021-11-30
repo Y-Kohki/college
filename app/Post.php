0161629e5b1a6f8ed5f29a915639d992514b4a7f
Y-Kohki <kohki000608@yahoo.co.jp>
2021-11-30 21:43:10 +0900

    [add]記事投稿画面

---
+++ app/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Post extends Model
+{
+
+public function getPaginateByLimit(int $limit_count = 10)
+{
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+}
+public function getByLimit(int $limit_count = 10)
+{
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+    return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+}
+
+protected $fillable = [
+    'title',
+    'body',
+];
+
+}
