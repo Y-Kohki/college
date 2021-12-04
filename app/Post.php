af9ead072ed055b279eb8522a9ef68fc7d952b21
Y-Kohki <kohki000608@yahoo.co.jp>
2021-12-04 20:56:26 +0900

    [add]記事削除画面

---
+++ app/Post.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use SoftDeletes;
+
+public function getPaginateByLimit(int $limit_count = 10)
+{
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+}
+public function getByLimit(int $limit_count = 10)
+{
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+    return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+}
+
+protected $fillable = [
+    'title',
+    'body',
+];
+
+public function getPaginate()
+{
+    return $this->orderBy('updated_at','DESC')->paginate(5);
+}
+
+}
